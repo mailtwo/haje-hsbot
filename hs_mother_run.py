a35bb1c9bb5ac5130f8109cb29b2a70780244753
Jungpyo Hong <3455679+mailtwo@users.noreply.github.com>
2018-04-21 15:07:24 +0900

    Minor change

--- hs_mother_run.py
+++ hs_mother_run.py
@@ -8,7 +8,7 @@ cur_os_type = 'linux'
 if sys.platform.startswith('win'):
     cur_os_type = 'win'
 
-def process_message(mode, sc, proc, msg, user=None):
+def process_message(mode, sc, channel_id, proc, msg, user=None):
     argv = msg.strip().split()
     if argv[0] == '종료':
         if proc is None:
@@ -64,6 +64,7 @@ def process_message(mode, sc, proc, msg, user=None):
         sc.api_call(
             'chat.postMessage',
             username='하스봇엄마',
+            channel=channel_id,
             user=user,
             text=f_str
         )
@@ -73,6 +74,7 @@ def process_message(mode, sc, proc, msg, user=None):
             f_str = f.read()
         sc.api_call(
             'chat.postMessage',
+            channel=channel_id,
             username='하스봇엄마',
             user=user,
             text=f_str
@@ -120,7 +122,7 @@ def main():
             op = '하스봇엄마!'
             if text[:len(op)] != op:
                 continue
-            proc = process_message(mode, sc, proc, text[len(op):], user=msg_info['user'])
+            proc = process_message(mode, sc, channel_id, proc, text[len(op):], user=msg_info['user'])
 
 
 if __name__ == '__main__':
